e45bc35df75fa78a6456d10b2fec518c1850f215
LeJin27 <jiqle@ucsc.edu>
2024-12-12 16:51:40 -0800

    add read_items with specific queries

--- backend/app.py
+++ backend/app.py
@@ -1,41 +1,88 @@
 from spotify_to_database import SpotifyToDatabase
-from spotify_interface import SpotifyInterface
-from youtube_interface import YoutubeInterface
 from database_interface_pg import DatabaseInterfacePg
 from database_interface_sqlite import DatabaseInterfaceSqlite
-
+from spotify_interface import helper_prettify
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+from pydantic import create_model
+from typing import Optional, List
 
 
 def postgres():
-    pg_db = DatabaseInterfacePg(host="localhost", dbname="songs", user="postgres", password="dog", port=5432)
+    pg_db = DatabaseInterfacePg(
+        host="localhost", dbname="songs", user="postgres", password="dog", port=5432)
     pg_schema_json = {
-       "spotify_key": "SERIAL PRIMARY KEY", 
-       "spotify_url" : "TEXT",
-       "spotify_name": "VARCHAR(255)",
-       "spotify_artists": "VARCHAR(255)[]", 
-       "spotify_yt_query": "TEXT", 
-       "youtube_url": "TEXT",
-       "youtube_name": "VARCHAR(255)",
-       "youtube_length": "NUMERIC",
-       "youtube_path": "TEXT",
+        "spotify_key": "SERIAL PRIMARY KEY",
+        "spotify_url": "TEXT",
+        "spotify_name": "VARCHAR(255)",
+        "spotify_artists": "VARCHAR(255)[]",
+        "spotify_yt_query": "TEXT",
+        "youtube_url": "TEXT",
+        "youtube_name": "VARCHAR(255)",
+        "youtube_length": "NUMERIC",
+        "youtube_path": "TEXT",
     }
     pg_db.create_table("songs", schema_json=pg_schema_json)
-    spotData = SpotifyToDatabase(pg_db)
-    spotData.download_from_playlist("https://open.spotify.com/playlist/3EL0PRZNjtWmFFLXiAgb2b?si=5560SJQcTPGDZHEtO4KViw")
+    spot_data = SpotifyToDatabase(pg_db)
+    spot_data.download_from_playlist(
+        "https://open.spotify.com/playlist/3EL0PRZNjtWmFFLXiAgb2b?si=5560SJQcTPGDZHEtO4KViw")
+
+
+class song_base(BaseModel):
+    spotify_key: Optional[int] = None
+    spotify_url: Optional[str] = None
+    spotify_name: Optional[str] = None
+    spotify_artists: Optional[str] = None
+    spotify_yt_query: Optional[str] = None
+    youtube_url: Optional[str] = None
+    youtube_name: Optional[str] = None
+    youtube_length: Optional[int] = None
+    youtube_path: Optional[str] = None
+
+def convert_dict_list_to_models(dict_list: List[dict]) -> List[song_base]:
+    """
+    Converts a list of dictionaries to a list of SongBase models.
+
+    :param dicts: A list of dictionaries.
+    :return: A list of SongBase instances.
+    """
+    return [song_base(**dict_entry) for dict_entry in dict_list]
 
 
 sqlite_db = DatabaseInterfaceSqlite("media_compiler")
 sqlite_json_schema = {
-   "spotify_key": "INTEGER PRIMARY KEY", 
-   "spotify_url" : "TEXT",
-   "spotify_name": "TEXT",
-   "spotify_artists": "TEXT", 
-   "spotify_yt_query": "TEXT", 
-   "youtube_url": "TEXT",
-   "youtube_name": "TEXT",
-   "youtube_length": "INTEGER",
-   "youtube_path": "TEXT",
+    "spotify_key": "INTEGER PRIMARY KEY",
+    "spotify_url": "TEXT",
+    "spotify_name": "TEXT",
+    "spotify_artists": "TEXT",
+    "spotify_yt_query": "TEXT",
+    "youtube_url": "TEXT",
+    "youtube_name": "TEXT",
+    "youtube_length": "INTEGER",
+    "youtube_path": "TEXT",
 }
 sqlite_db.create_table("songs", sqlite_json_schema)
-spotData = SpotifyToDatabase(sqlite_db)
-spotData.download_from_playlist("https://open.spotify.com/playlist/3GhsvsVomUEGcVk33Jb8L6?si=GcVSv4tKTrCw_YxW69gSig")
+spot_data = SpotifyToDatabase(sqlite_db)
+
+app = FastAPI()
+
+
+def _filter_dict_valid_key(dict):
+    filtered_dict = {k: v for k, v in dict.items() if v is not None}
+    return filtered_dict
+
+
+@app.get("/songs/")
+async def read_items(params: song_base = Depends()):
+    # remove none values in song_base and convert to dictioanry
+    params_dict = params.model_dump()
+    filtered_params = _filter_dict_valid_key(params_dict)
+
+    all_songs = sqlite_db.get_items_from_table("songs", filtered_params)
+    models = convert_dict_list_to_models(all_songs)
+
+    return models
+
+
+
+
--- backend/database_interface_sqlite.py
+++ backend/database_interface_sqlite.py
@@ -2,6 +2,7 @@
 import sqlite3
 import psycopg2
 
+
 class DatabaseInterfaceSqlite:
     def __init__(self, dbname):
         self.connection = sqlite3.connect("songs.db")
@@ -13,8 +14,9 @@ class DatabaseInterfaceSqlite:
         Args:
             table_name (string): Name of table
             schema_json (dict): Dict containing schema columns and their types
-        """      
-        table_schema = ', '.join(" ".join((col_name, data_type)) for (col_name, data_type) in schema_json.items())
+        """
+        table_schema = ', '.join(" ".join((col_name, data_type))
+                                 for (col_name, data_type) in schema_json.items())
         try:
             # Dynamically construct the CREATE TABLE SQL query
             sql_query = f"""
@@ -28,36 +30,40 @@ class DatabaseInterfaceSqlite:
             print(f"Table {table_name} created successfully.")
         except Exception as e:
             print(f"An error occurred: {e}")
-   
-   
-    def get_items_from_table(self, table_name, dynamic_query = None):
-       """Returns items that match a given query from a table namw
 
-       Args:
-           table_name (string): Table name to get items from
-           dynamic_query (dict, optional): Query with optional filters
+    def get_items_from_table(self, table_name, dynamic_query=None):
+        """Returns items that match a given query from a table namw
 
-       Returns:
-           [dict]: A list of items returned from query 
-       """      
+        Args:
+            table_name (string): Table name to get items from
+            dynamic_query (dict, optional): Query with optional filters
 
-       try: 
+        Returns:
+            [dict]: A list of items returned from query 
+        """
 
-          sql_query = f"SELECT * FROM {table_name}"
+        try:
+            self.connection.row_factory = sqlite3.Row
+            self.cursor = self.connection.cursor()
 
-          if (dynamic_query != None):
-             sql_query += " WHERE "
-             table_schema = ' AND '.join(f"{json_key} = '{json_value}'" for (json_key, json_value) in dynamic_query.items())
-             sql_query += table_schema
+            sql_query = f"SELECT * FROM {table_name}"
 
-          self.cursor.execute(sql_query)
-          items = self.cursor.fetchall()
-          return items
+            dynamic_query_is_not_empty = dynamic_query != None and len(dynamic_query) != 0
+            if (dynamic_query_is_not_empty):
+                sql_query += " WHERE "
+                table_schema = ' AND '.join(f"{json_key} = '{json_value}'" for (
+                    json_key, json_value) in dynamic_query.items())
+                sql_query += table_schema
 
-       except Exception as e:
-          print("Error getting items from table")
-          return []
-   
+            self.cursor.execute(sql_query)
+            # conversion to dicitonary
+            items = [dict(row) for row in self.cursor.fetchall()]
+            return items
+
+        except Exception as e:
+            print("Error getting items from table")
+            return []
+
     def insert_into_table(self, table_name, row_json):
         """Inserts into table a json item
 
@@ -66,49 +72,44 @@ class DatabaseInterfaceSqlite:
             row_json (dict): A dict containing keys and values of insertion
         """
         for key, value in row_json.items():
-          if isinstance(value, list):
-              row_json[key] = ', '.join(value)
+            if isinstance(value, list):
+                row_json[key] = ', '.join(value)
 
         json_keys = list(row_json.keys())
         json_values = list(row_json.values())
 
-        value_placeholders = ', '.join(['?'] * len(json_keys))  # Generate placeholders %s, %s, %s, %s
-        column_names = ', '.join(json_keys)  # Join column names into a single string for sql query
+        # Generate placeholders %s, %s, %s, %s
+        value_placeholders = ', '.join(['?'] * len(json_keys))
+        # Join column names into a single string for sql query
+        column_names = ', '.join(json_keys)
 
         try:
-             sql_query = f"""
-                         INSERT INTO {table_name} 
-                            ({column_names}) 
-                            VALUES 
-                            ({value_placeholders})
-                         """
-             self.cursor.execute(sql_query, json_values)
-             self.connection.commit()  # Make sure changes are saved to the database
+            sql_query = f"""
+                        INSERT INTO {table_name}
+                           ({column_names})
+                           VALUES
+                           ({value_placeholders})
+                        """
+            self.cursor.execute(sql_query, json_values)
+            self.connection.commit()  # Make sure changes are saved to the database
 
         except Exception as e:
-           print("Insertion gone wrong {e}", e)
+            print("Insertion gone wrong {e}", e)
 
-#dog = DatabaseInterfaceSqlite("songs")
+# dog = DatabaseInterfaceSqlite("songs")
 #
-#json_schema = {
-#   "spotify_key": "INTEGER PRIMARY KEY", 
+# json_schema = {
+#   "spotify_key": "INTEGER PRIMARY KEY",
 #   "spotify_url" : "TEXT",
 #   "spotify_name": "TEXT",
-#   "spotify_yt_query": "TEXT", 
+#   "spotify_yt_query": "TEXT",
 #   "youtube_url": "TEXT",
 #   "youtube_name": "TEXT",
 #   "youtube_length": "INTEGER",
 #   "youtube_path": "TEXT",
-#}
-#dog.create_table("songs", json_schema)
+# }
+# dog.create_table("songs", json_schema)
 #
-#    
 #
-#print(dog.get_tables())
-    
-    
-
-    
-
-  
-  
+#
+# print(dog.get_tables())
