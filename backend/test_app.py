fd59f1572bd611875fccafbd77f6cfb146479f63
LeJin27 <jiqle@ucsc.edu>
2024-12-18 12:40:13 -0800

    added testing file for testing non fastapi related activiteis

--- backend/test_app.py
+++ backend/test_app.py
@@ -1,3 +1,4 @@
+
 from spotify_to_database import SpotifyToDatabase
 from database_interface_pg import DatabaseInterfacePg
 from database_interface_sqlite import DatabaseInterfaceSqlite
@@ -44,41 +45,4 @@ sqlite_json_schema = {
 }
 sqlite_db.create_table("songs", sqlite_json_schema)
 spot_data = SpotifyToDatabase(sqlite_db)
-#spot_data.download_from_playlist("https://open.spotify.com/playlist/07xdE0QkcFKOi8sQCcsMcz?si=e9823f3ebb7d439e")
-
-app = FastAPI()
-origins = [
-    "http://localhost",
-    "http://localhost:8080",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-def _filter_dict_valid_key(dict):
-    filtered_dict = {k: v for k, v in dict.items() if v is not None}
-    return filtered_dict
-
-@app.get("/songs/")
-async def read_items(params: song_base = Depends()):
-    # remove none values in song_base and convert to dictioanry
-    params_dict = params.model_dump()
-    print(f"Received params: {params_dict}")  # Logs incoming parameters
-    filtered_params = _filter_dict_valid_key(params_dict)
-
-    all_songs = sqlite_db.get_items_from_table("songs", filtered_params)
-    models = convert_dict_list_to_models(all_songs)
-
-    return models
-
-
-
-@app.get("/audio")
-async def read_audio():
-    audio_path = "./music/Persona 5 OST 04 - Life Will Change "  # Replace with the actual path to your audio file
-    return FileResponse(audio_path, media_type="audio/mpeg")
+helper_prettify(sqlite_db.get_items_from_table("songs"))
